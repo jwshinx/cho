41d3115c43107a296f5defa97175b717f1af8a81
Joel Shin <joel.shin@here.com>
2014-09-08 17:41:03 -0700

    hit endpoint

--- cho.rb
+++ cho.rb
@@ -1,11 +1,20 @@
 require "rubygems"
 require "sinatra/base"
 require 'haml'
+require 'oauth2'
+require 'httparty'
 
 class Cho < Sinatra::Base
 
   get '/' do
-    'Hello, nginx and unicorn!'
+    my_token = ENV['DK_ACCESS_TOKEN']
+    endpoint = ENV['DK_ENDPOINT']
+    resp = HTTParty.get(endpoint + "?access_token=" + my_token)
+    
+    output = resp.code.to_s            + "<br /><br /><br />" \
+      + resp.message.to_s              + "<br /><br /><br />" \
+      + resp.headers.inspect           + "<br /><br /><br />" \
+      + JSON.parse(resp.body).inspect  + "<br /><br /><br />"
   end
 
   get '/weather' do
@@ -23,7 +32,6 @@ class Cho < Sinatra::Base
   end
 
   get '/dots/:id' do
-
+    "single dot: #{params[:id]}"
   end
 end
-
